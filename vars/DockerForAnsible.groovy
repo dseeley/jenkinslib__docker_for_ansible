8c9ea98f93ef652c01183b5ad8b48b166c8730e3
Dougal Seeley <git@dougalseeley.com>
2024-12-01 15:30:45 +0000

    Update to fix dependency issue, add ansible version

--- vars/DockerForAnsible.groovy
+++ vars/DockerForAnsible.groovy
@@ -1,25 +1,33 @@
 #!groovy
 
-def call(String image_name, String build_opts = "") {
+def call(String image_name, String build_opts = "", String ansible_version = "") {
+    // Create a lock to prevent building the same image in parallel
     lock("IMAGEBUILDLOCK__${image_name}__${env.NODE_NAME}") {
         def jenkins_username = sh(script: 'whoami', returnStdout: true).trim()
         def jenkins_uid = sh(script: "id -u ${jenkins_username}", returnStdout: true).trim()
         def jenkins_gid = sh(script: "id -g ${jenkins_username}", returnStdout: true).trim()
 
+        if (ansible_version == "") {
+            def pypi_ansible = ["curl", "-s", "-H", "Accept: application/json", "-H", "Content-type: application/json", "GET", "https://pypi.org/pypi/ansible/json"].execute().text
+            def pypi_ansible_latest = new groovy.json.JsonSlurper().parseText(pypi_ansible).info.version        // Use `info.version` instead of `.releases.keySet()[-1]`, to avoid alpha and release candidate versions
+            ansible_version = "${pypi_ansible_latest}"
+        }
+
         def dockerfile = """
             FROM ubuntu:24.04
             ARG DEBIAN_FRONTEND=noninteractive
-            ENV JENKINS_HOME=${env.JENKINS_HOME}
-            ENV HOME=${env.JENKINS_HOME}
-            ENV TZ=Europe/London
+            ENV TZ=UTC
             SHELL ["/bin/bash", "-c"]
 
             RUN groupadd -g ${jenkins_gid} ${jenkins_username} && useradd -m -u ${jenkins_uid} -g ${jenkins_gid} -s /bin/bash ${jenkins_username}
-            RUN apt-get update \
-                && apt-get install -y git iproute2 python3-pip python3-jmespath python3-jinja2 python3-boto3 python3-netaddr python3-paramiko python3-libvirt python3-lxml python3-xmltodict python3-pycdlib python3-google-auth python3-dev python3-setuptools python3-wheel \
-                && pip3 --no-cache-dir install --break-system-packages ansible
-
-            RUN pip3 --no-cache-dir install --break-system-packages -r \$(pip3 --no-cache-dir show ansible | grep ^Location | sed -r 's/^Location: (.*)/\\1/')/ansible_collections/azure/azcollection/requirements*.txt
+            
+            # TZ Stuff is for python install
+            # Install python3, pip, then all the azure pip packages, *then* the apt packages (otherwise conflicting pip packages will fail to install)
+            RUN ln -snf /usr/share/zoneinfo/\$TZ /etc/localtime && echo \$TZ > /etc/timezone \
+                && apt-get update && apt-get install -y git iproute2 python3 python3-pip \
+                && pip3 --no-cache-dir install --break-system-packages ansible==${ansible_version} \
+                && pip3 --no-cache-dir install --break-system-packages -r \$(pip3 --no-cache-dir show ansible | grep ^Location | sed -r 's/^Location: (.*)/\\1/')/ansible_collections/azure/azcollection/requirements*.txt \
+                && apt-get install -y python3-jmespath python3-jinja2 python3-boto3 python3-netaddr python3-paramiko python3-libvirt python3-lxml python3-xmltodict python3-pycdlib python3-google-auth python3-dev python3-setuptools python3-wheel
         """.stripIndent()
 
         writeFile(file: "Dockerfile_${image_name}", text: dockerfile, encoding: "UTF-8")
